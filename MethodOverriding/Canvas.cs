bd9924e46b8dbc41ad58f2248149d39960fc0b51
Galante <fabiogalantemans@gmail.com>
2020-03-10 12:51:46 -0300

    Add project files.

---
+++ MethodOverriding/Canvas.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    public class Canvas
+    {
+        public void DrawShapres(List<Shape> shapes)
+        {
+            //Bad solution, add for each form
+            foreach (var shape in shapes)
+            {
+                switch (shape.Type)
+                {
+                    case ShapeType.Circle:
+                       Console.WriteLine("Draw a circle");
+                        break;
+
+                    case ShapeType.Rectangle:
+                        Console.WriteLine("Draw a rectangle");
+                        break;
+
+                    case ShapeType.Triangle:
+                        Console.WriteLine("Draw a triangle");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MethodOverriding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var shapes = new List<Shape>();
+            shapes.Add(new Shape{Width = 100, Height = 100, Type = ShapeType.Circle});
+            shapes.Add(new Shape { Width = 100, Height = 25, Type = ShapeType.Triangle });
+            shapes.Add(new Shape { Width = 100, Height = 30, Type = ShapeType.Rectangle });
+
+            var canvas = new Canvas();
+            canvas.DrawShapres(shapes);
+
+        }
+    }
+}
